5fcac8e5a446ec768bd563cd7df4bfbe1e9517f1
ptuananhne <phamtuananh02dl@gmail.com>
2025-08-08 12:50:59 +0700

    xz

--- server/middleware/adminMiddleware.js
+++
@@ -1,9 +0,0 @@
-const admin = (req, res, next) => {
-  if (req.user && req.user.role === 'admin') {
-    next();
-  } else {
-    res.status(401).json({ message: 'Không có quyền truy cập, yêu cầu quyền Admin.' });
-  }
-};
-
-module.exports = { admin };
